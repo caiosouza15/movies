d7676b1d19a9f91728c1ba89e8e041be518f373e
caiosoza15 <caiosouza.15@outlook.com>
2022-01-15 10:45:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, useEffect, useState} from 'react';
+import { Movie } from './types/Movie';
+
+function app(){    
+
+  const [movies, setMovies] = useState<Movie[]>([]);  
+
+  const loadMovies = () => {   
+      fetch('https://api.b7web.com.br/cinema/').then((res) =>{
+        return res.json();
+    })
+    .then((json)=>{
+      setMovies(json);
+    })
+  
+} 
+  return (    
+   <div>  
+     <div className="h-24 flex flex-col items-center justify-center">  
+     <button className="w-40 bg-cyan-100 p-2 flex justify-center my-3 drop-shadow-xl rounded" onClick={loadMovies}>Carregar filmes</button>
+      Total de filmes:{movies.length}
+     </div>     
+     <div className="grid grid-cols-6 mt-4">
+       {movies.map((item, index) => (
+         <div className="mx-3 m-4 text-center drop-shadow-2xl"> 
+           <img src={item.avatar} className="w-50 block"/>
+           {item.titulo}
+         </div>
+       ))}
+     </div>
+   </div>
+  );
+}
+export default app;
